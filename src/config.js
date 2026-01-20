4de6f34b9ad3d5a147de6e9f8618d0a4ca6f0b8f
Rushi <apple@Apples-MacBook-Air.local>
2026-01-20 11:27:53 +0530

    fix: hardcode api url to bypass cache

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const API_URL = import.meta.env.VITE_API_URL || `http://${window.location.hostname}:5002`;
+export const API_URL = import.meta.env.PROD
+    ? 'https://api.astropravin.com'
+    : (import.meta.env.VITE_API_URL || `http://${window.location.hostname}:5002`);
